db34fe18bb2db16a19c9cf286bd0d1c8e5f1dd06
Eduardo Santizo <eddysanoli@gmail.com>
2022-10-30 14:30:46 -0600

    Added a Server Config File (WIP)
    The start_server script was failing because I added arguments to it. So I decided to remove the arguments and add the parameters that were there into a server config file for the server itself. I thought it would work, but the server keeps prompting me for settings each time I run it. I will have to properly check how to make the server run with those configs.
    
    Other changes:
    - Made it so that the worlds that are copied into the server are able to be written, read and executed for any user. Basically changed their permissions.

--- ansible/storage/terraria/start_server.sh
+++ ansible/storage/terraria/start_server.sh
@@ -1,6 +1,2 @@
 # Run the Terraria Server Executable
-# - Use the Noobtopia 2 world
-# - Allow a max of 5 players
-./1447/Linux/TerrariaServer.bin.x86_64 \
-    -world "/home/ubuntu/.local/share/Terraria/Worlds/Noobtopia2-ElectricBoogaloo.wld" \
-    -maxplayers 5 
+./1447/Linux/TerrariaServer.bin.x86_64 
