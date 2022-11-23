ee9a85c7b722728762a568c677f4edbb6f04c9dd
Eduardo Santizo <eddysanoli@gmail.com>
2022-11-22 22:26:43 -0600

    Parametrized the Terraria Version
    Terraria just had a minor update, causing us to not be able to access the server, since the server was on version 1447 and our game was on version 1449. So, I decided to parametrize the Terraria version in the provisioning playbook, so that in the future, in case the game updates again, I can just change the value of the variable and everything should run correctly when doing "./start_server.sh". Pretty cool.

--- ansible/storage/terraria/start_server.sh
+++ ansible/storage/terraria/start_server.sh
@@ -1,2 +1,6 @@
 # Run the Terraria Server Executable
-./1447/Linux/TerrariaServer.bin.x86_64 -config serverconfig.txt
+# NOTE: The "TERRARIA_VERSION" variable is replaced by the actual version
+# (1.4.4.9 -> 1449) when the "provisioning.yaml" Ansible playbook runs. It 
+# will replace the variable by the "terraria_version" variable specified at 
+# the top of the YAML file.
+./$TERRARIA_VERSION/Linux/TerrariaServer.bin.x86_64 -config serverconfig.txt
